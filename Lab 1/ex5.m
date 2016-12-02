b089d49e48937894cb9cf5fdae4b005b67957df0
Stefan Bussemaker <sbussemaker@gmail.com>
2016-12-02 14:44:39 +0100

    Lab 1 Ex 5, 7

--- Lab 1/ex5.m
+++ Lab 1/ex5.m
@@ -1,9 +1,40 @@
+% Vincent de Wit (s3038858)
+% Stefan Bussemaker (s2004674)
 function samples = sinusoid2samples(amplitude, omega, phi, samplerate)
-% put here the code from exercise 3
+  samples = zeros(1,samplerate);
+	for i = 0:(samplerate)
+		samples(i+1) = amplitude * cos (omega*i/samplerate + phi);
+	end
+end
+
+function phi=samples2phase(omega, x, samplerate)
+  [~, t_m] = max(x);          % Find first peak
+  t_m = t_m - 1;              % Fix index
+  t_m = t_m / samplerate;     % Convert array index to time index
+  phi = -omega * t_m;         % Compute phi
+  phi = mod(phi, 2*pi);       % Remove multiples of 2pi
+  if (phi > pi)               % Translate phi from [0, 2pi)
+    phi = phi - 2*pi;         %   to (-pi, pi], so that
+  end                         %   -pi < phi <= pi
 end
 
 function [amplitude,omega,phi]=samples2sinusoid(x, samplerate)
-%implement this function yourself
+  % Amplitude
+  amplitude = (max(x) - min(x)) / 2;
+  
+  % Omega
+  peaks = find(x == max(x));          % Find all peaks
+  peaks = peaks - 1;                  % Find indices
+  t_1 = peaks(1); t_2 = peaks(2);
+
+  T = (t_2 - t_1)
+  f_hat = 1 / (t_2 - t_1)
+  w_hat = f_hat * 2*pi
+  T_s = 1 / samplerate
+  omega = w_hat / T_s
+  
+  % Phi
+  phi = samples2phase(omega, x, samplerate)
 end
 
 %%%%%%% main program starts here %%%%%%%
--- Lab 1/ex7.m
+++ Lab 1/ex7.m
@@ -1,5 +1,31 @@
+% Vincent de Wit (s3038858)
+% Stefan Bussemaker (s2004674)
+function [theta,r] = cart2polar(x,y)
+	theta = atan2(y, x);
+	r = sqrt(x*x + y*y);
+end
+
+function [x,y] = polar2cart(theta, r)
+	x = r * cos(theta);
+	y = r * sin(theta);
+end
+
+function [omega2,A2,phi2] = addSinusoids(omega, A0, phi0, A1, phi1)
+  omega2 = omega;
+  [x1, y1] = polar2cart(phi0, A0);
+  [x2, y2] = polar2cart(phi1, A1);
+  [phi2, A2] = cart2polar(x1+x2, y1+y2);
+end
+
 function [omega2,A2,phi2] = microphoneSignal(omega, x0, y0, A0, phi0, x1, y1, A1, phi1, x2, y2)
-% implement this function yourself
+  sound_speed = 343;
+  dist0 = norm([x0, y0] - [x2, y2], 2);     % distance from 0 to 2
+  dist1 = norm([x1, y1] - [x2, y2], 2);     % distance from 0 to 2
+  timediff_0 = dist0 / 343;
+  timediff_1 = dist1 / 343;
+  phi0 = phi0 - timediff_0 * omega;
+  phi1 = phi1 - timediff_1 * omega;
+  [omega2, A2, phi2] = addSinusoids(omega, A0, phi0, A1, phi1);
 end
   
 %%%%%%% main program starts here %%%%%%%
