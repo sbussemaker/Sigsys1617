2e27a85228ba7403685d059ba54ac053ac68cdec
Stefan Bussemaker <sbussemaker@gmail.com>
2016-12-05 12:45:37 +0100

    Lab 1 Ex 6

--- Lab 1/ex6.m
+++ Lab 1/ex6.m
@@ -1,5 +1,20 @@
+% Vincent de Wit (s3038858)
+% Stefan Bussemaker (s2004674)
+function [theta,r] = cart2polar(x,y)
+	theta = atan2(y, x);
+	r = sqrt(x*x + y*y);
+end
+
+function [x,y] = polar2cart(theta, r)
+	x = r * cos(theta);
+	y = r * sin(theta);
+end
+
 function [omega2,A2,phi2] = addSinusoids(omega, A0, phi0, A1, phi1)
-% implement this function yourself
+  omega2 = omega;                         % Omega stays the same
+  [x1, y1] = polar2cart(phi0, A0);        % Convert to cartesian
+  [x2, y2] = polar2cart(phi1, A1);        % Convert to cartesian
+  [phi2, A2] = cart2polar(x1+x2, y1+y2);  % Add, convert back to polar
 end
 
 %%%%%%% main program starts here %%%%%%%
